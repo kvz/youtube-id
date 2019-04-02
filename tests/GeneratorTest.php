fdc260d723cf51fc09de1b4024bf38582483ea72
Alexey Samara <lion.samara@gmail.com>
2019-04-02 15:00:54 +0300

    initiated structure

---
+++ tests/GeneratorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Translates a number to a short alphanumeric version
+ *
+ * @author Kevin van Zonneveld <kevin@transloadit.com> (https://github.com/kvz)
+ * @author Simon Franz
+ * @author Deadfish
+ * @author SK83RJOSH
+ * @author Alexey Samara <lion.samara@gmail.com> (https://github.com/wow-apps)
+ * @copyright 2008 Kevin van Zonneveld.
+ */
+
+namespace Kvz\YoutubeId\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneratorTest extends TestCase
+{
+}
